8eeb9c3211a47c9c336c1c8ba8c5edb60ec8b821
Rameshwar Bedade <rambedade2308@gmail.com>
2023-08-17 23:40:47 +0530

    first commit

---
+++ src/components/Intro1.js
@@ -0,0 +1,13 @@
+function Intro1 (){
+    return (
+        <div class="blog-post-intro">
+            <h1>I've become a React Developer</h1>
+            <div>
+                <p>I've completed  the react course of the META  and now I am happy to say that I am Junior React Developer</p>
+                <p className="link">Read more ....</p>
+            </div>
+        </div>
+    )
+}
+
+export default Intro1
---
+++ src/components/Promo.js
@@ -0,0 +1,15 @@
+function Promo() {
+    return(
+        <div className="promo-section">
+            <div>
+                <h1>Don't miss the deal</h1>
+            </div>
+            <div>
+                <h1>Subscribr to my newsletter and get all the shop items at 50 % off!</h1>
+            </div>
+        </div>
+    )
+    
+}
+
+export default Promo
